958a8ed8178a8503191d77c5a3c2124ba4d0a8f6
Bryan Palma <bryanmauriciopalma@hotmail.com>
2022-03-01 15:07:17 -0600

    modelos y metodos de clase

--- src/java/Model/horas_usuario.java
+++ src/java/Model/horas_usuario.java
@@ -6,6 +6,9 @@
 package Model;
 
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -17,4 +20,37 @@ public class horas_usuario {
     public int id_usuario;
     public Date fecha;
     public float HorasTrabajadas;
+    public float salario_hora;
+    
+    public ArrayList<horas_usuario> GetHorasByUsuario(int id)throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
+        ocon = new Conexion();
+        ResultSet rs = ocon.consultar("*", "horas_usuario where id_usuario="+id);
+        ArrayList<horas_usuario> horas = new ArrayList<>();
+        while (rs.next()) {
+            horas_usuario item = new horas_usuario();
+            item.id_usuario=rs.getInt("id_usuario");
+            item.id_horas_usuario=rs.getInt("id_horas_usuario");
+            item.fecha=rs.getDate("fecha");
+            item.HorasTrabajadas=rs.getFloat("horas_trabajadas");
+            item.salario_hora=rs.getFloat("salario_hora");
+            horas.add(item);
+        }
+        return horas;
+    }
+    public static int Add(horas_usuario item)throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException{
+        ocon = new Conexion();
+        String query = "insert into horas_usuario(id_usuario,fecha,horas_trabajadas,salario_hora) values("+item.id_usuario+",'"+item.fecha+"',"+item.HorasTrabajadas+","+item.salario_hora+");";
+        int resp = ocon.agregar(query);
+        return resp;
+    }
+     public static void update(horas_usuario item) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
+        ocon = new Conexion();
+        String query = "update usuario set fecha='"+item.fecha+"',horas_trabajadas='"+item.HorasTrabajadas+"',salario_horas = '"+item.salario_hora+"'";
+        ocon.DoQuery2(query);
+    }
+     public static int Delete(int id)throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException{
+        ocon = new Conexion();
+        return ocon.eliminar("horas_usuario", "id_horas_usuario="+id);
+    }
+    
 }
